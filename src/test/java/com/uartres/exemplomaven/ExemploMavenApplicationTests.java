628ce2fbbdc7ac82d31d1ca3a123bdb9e9e411f0
Sergio Jose Santos Junior <hagare@gmail.com>
2021-06-14 08:26:55 -0300

    Commit inicial

---
+++ src/test/java/com/uartres/exemplomaven/ExemploMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uartres.exemplomaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExemploMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
